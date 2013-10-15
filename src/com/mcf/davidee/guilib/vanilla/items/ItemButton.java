f27681d1c0952c291f2c520e655f4b3e19f64e3d
David Goldman <dallasftball@gmail.com>
2013-10-15 11:10:49 -0400

    ItemButton from MinecraftScripting; v1.0.6.2

---
+++ src/com/mcf/davidee/guilib/vanilla/items/ItemButton.java
@@ -0,0 +1,90 @@
+package com.mcf.davidee.guilib.vanilla.items;
+
+import java.util.Arrays;
+import java.util.List;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.item.ItemStack;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+
+import com.mcf.davidee.guilib.core.Button;
+import com.mcf.davidee.guilib.core.Scrollbar.Shiftable;
+import com.mcf.davidee.guilib.core.Widget;
+
+/**
+ * 
+ * This class is a Widget copy of a vanilla item button. 
+ * This button supports "Air" - an item with ID 0.
+ * 
+ * Note that items use zLevel for rendering - change zLevel as needed.
+ *
+ */
+public class ItemButton extends Button implements Shiftable {
+
+	public static final int WIDTH = 18;
+	public static final int HEIGHT = 18;
+	public static final RenderItem itemRenderer = new RenderItem();
+
+	protected ItemStack item;
+	protected List<Widget> tooltip;
+
+	private GuiScreen parent;
+	protected boolean hover;
+
+	public ItemButton(ItemStack item, ButtonHandler handler) {
+		super(WIDTH, HEIGHT, handler);
+
+		this.parent = mc.currentScreen;
+		this.zLevel = 100;
+		setItem(item);
+	}
+
+	protected void setItem(ItemStack item) {
+		if (item.getItem() == null && item.itemID != 0)
+			throw new IllegalArgumentException("Item to display does not exist");
+		this.item = item;
+		this.tooltip = Arrays.asList((Widget)new ItemTooltip(item, parent));
+	}
+
+	/**
+	 * Draws the item or string "Air" if itemID = 0.
+	 */
+	@Override
+	public void draw(int mx, int my) {
+		hover = inBounds(mx, my);
+		if (hover) {
+			GL11.glDisable(GL11.GL_DEPTH_TEST);
+			drawRect(x, y, x + width, y + height, 0x55909090);
+			GL11.glEnable(GL11.GL_DEPTH_TEST);
+			tooltip.get(0).setPosition(mx, my);
+		}
+		if (item.itemID != 0) {
+			RenderHelper.enableGUIStandardItemLighting();
+			OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
+			GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+			itemRenderer.zLevel = this.zLevel;
+			itemRenderer.renderItemAndEffectIntoGUI(mc.fontRenderer, mc.getTextureManager(), item, x + 1, y + 1);
+			itemRenderer.zLevel = 0;
+			GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+			RenderHelper.disableStandardItemLighting();
+		}
+		else //Air
+			drawString(mc.fontRenderer, "Air" , x + 3, y + 5, -1);
+	}
+
+
+	@Override
+	public List<Widget> getTooltips() {
+		return (hover) ? tooltip : super.getTooltips();
+	}
+
+	@Override
+	public void shiftY(int dy) {
+		this.y += dy;
+	}
+}
---
+++ src/com/mcf/davidee/guilib/vanilla/items/ItemTooltip.java
@@ -0,0 +1,137 @@
+package com.mcf.davidee.guilib.vanilla.items;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockEndPortal;
+import net.minecraft.block.BlockPistonExtension;
+import net.minecraft.block.BlockPistonMoving;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+
+import org.lwjgl.opengl.GL11;
+
+import com.mcf.davidee.guilib.core.Widget;
+
+/**
+ * 
+ * This class represents a Widget copy of an Item's tooltip. 
+ * 
+ */
+public class ItemTooltip extends Widget {
+	
+	public static final char SECTION = '\u00a7';
+	
+	public static final Map<Class<?>, String> NAME_MAP = new HashMap<Class<?>, String>();
+	
+	static {
+		NAME_MAP.put(BlockPistonExtension.class, "Piston Extension");
+		NAME_MAP.put(BlockPistonMoving.class, "Piston Moving");
+		NAME_MAP.put(BlockEndPortal.class, "End Portal");
+	}
+	
+	private static String getUnknownName(ItemStack stack) {
+		Item item = stack.getItem();
+		if (item instanceof ItemBlock) {
+			int id = ((ItemBlock)item).getBlockID();
+			Class c = Block.blocksList[id].getClass();
+			return NAME_MAP.containsKey(c) ? NAME_MAP.get(c) : "Unknown";
+		}
+		return "Unknown";
+	}
+
+	private final List<String> tooltips;
+	private final FontRenderer font;
+	private final GuiScreen parent;
+
+	/**
+	 * See {@link net.minecraft.client.gui.inventory.GuiContainer#drawScreen} for more information.
+	 */
+	public ItemTooltip(ItemStack stack, GuiScreen parent) {
+		super(0, 0);
+
+		if (stack.itemID != 0) {
+			tooltips = stack.getTooltip(mc.thePlayer, mc.gameSettings.advancedItemTooltips);
+			if (!tooltips.isEmpty()) {
+				String name = tooltips.get(0);
+				if (name.startsWith("tile.null.name")) 
+					name = name.replace("tile.null.name", getUnknownName(stack));
+				tooltips.set(0, SECTION + Integer.toHexString(stack.getRarity().rarityColor) + name);
+				for (int i = 1; i < tooltips.size(); ++i)
+					tooltips.set(i, EnumChatFormatting.GRAY + tooltips.get(i));
+			}
+			FontRenderer itemRenderer = stack.getItem().getFontRenderer(stack);
+			font = (itemRenderer == null) ? mc.fontRenderer : itemRenderer;
+		}
+		else {
+			tooltips = Arrays.asList("Air");
+			font = mc.fontRenderer;
+		}
+		this.parent = parent;
+		this.width = getMaxStringWidth();
+		this.height = (tooltips.size() > 1) ? tooltips.size()*10 : 8; 
+	}
+
+	@Override
+	public void setPosition(int newX, int newY) {
+		this.x = newX + 12;
+		this.y = newY - 12;
+		if (x + width + 6 > parent.width)
+			x -= 28 + width;
+		if (y + height + 6 > parent.height)
+			y = parent.height - height - 6;
+	}
+
+
+	/**
+	 * See {@link net.minecraft.client.gui.inventory.GuiContainer#drawHoveringText}
+	 */
+	@Override
+	public void draw(int mx, int my) { 
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		if (!tooltips.isEmpty()) {
+			final int outlineColor = 0xf0100010;
+			drawRect(x - 3, y - 4, x + width + 3, y - 3, outlineColor);
+			drawRect(x - 3, y + height + 3, x + width + 3, y + height + 4, outlineColor);
+			drawRect(x - 3, y - 3, x + width + 3, y + height + 3, outlineColor);
+			drawRect(x - 4, y - 3, x - 3, y + height + 3, outlineColor);
+			drawRect(x + width + 3, y - 3, x + width + 4, y + height + 3, outlineColor);
+			int gradient1 = 1347420415;
+			int gradient2 = (gradient1 & 16711422) >> 1 | gradient1 & -16777216;
+			drawGradientRect(x - 3, y - 3 + 1, x - 3 + 1, y + height + 3 - 1, gradient1, gradient2);
+			drawGradientRect(x + width + 2, y - 3 + 1, x + width + 3, y + height + 3 - 1, gradient1, gradient2);
+			drawGradientRect(x - 3, y - 3, x + width + 3, y - 3 + 1, gradient1, gradient1);
+			drawGradientRect(x - 3, y + height + 2, x + width + 3, y + height + 3, gradient2, gradient2);
+			for (int index = 0; index < tooltips.size(); ++index) {
+				font.drawStringWithShadow(tooltips.get(index), x, y, -1);
+				if (index == 0)
+					y += 2;
+				y += 10;
+			}
+		}
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+	}
+
+	@Override
+	public boolean click(int mx, int my) {
+		return false;
+	}
+
+	private int getMaxStringWidth() {
+		int max = 0;
+		for (String s : tooltips) {
+			int width = font.getStringWidth(s);
+			if (width > max)
+				max = width;
+		}
+		return max;
+	}
+
+}
